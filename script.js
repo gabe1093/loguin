92fa5e8aee2f139e0cfb5d550aadfe80ae57fe5d
gabe1093 <gabrielrs1102@gmail.com>
2025-05-26 16:03:20 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+function enviar(){
+    
+//event.preventDefault()
+
+const swalWithBootstrapButtons = Swal.mixin({
+    customClass: {
+        confirmButton: "btn btn-success",
+      cancelButton: "btn btn-danger"
+    },
+    buttonsStyling: false
+  });
+  swalWithBootstrapButtons.fire({
+    title: "Desejá se cadastrar?",
+    text: "escolha",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonText: "Se cadastrar",
+    cancelButtonText: "Cancelar cadastro!",
+    reverseButtons: true
+  }).then((result) => {
+    if (result.isConfirmed) {
+      swalWithBootstrapButtons.fire({
+        title: "Cadastro",
+        text: "Você se cadastrou",
+        icon: "success"
+      });
+    } else if (
+      /* Read more about handling dismissals below */
+      result.dismiss === Swal.DismissReason.cancel
+    ) {
+      swalWithBootstrapButtons.fire({
+        title: "Cancelado",
+        text: "Cadastro cancelado ",
+        icon: "error"
+      });
+    }
+  });
+
+
+
+
+
+}
+
+
+
+
